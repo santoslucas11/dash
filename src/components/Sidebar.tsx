228ac99ff567e0ccec1144261bb1e98851bb9278
Lucas Rocha <santoslucas.dev@gmail.com>
2022-03-24 23:47:06 -0300

    finalizando sidebar e dashboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
-import Icon from "@chakra-ui/core/dist/Icon";
-import { Box, Stack, Text, Link } from "@chakra-ui/react";
-import { RiDashboardLine } from "react-icons/ri";
+import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
 
 export function Sidebar() {
     return (
@@ -9,9 +8,27 @@ export function Sidebar() {
                 <Box>
                     <Text fontWeight="bold" color="gray.400" fontSize="small"> GERAL</Text>
                     <Stack spacing="4" mt="8" align="stretch">
-                        <Link>
-                            <Icon as={RiDashboardLine} fontSize="25" />
-                            <Text ml="4" fontWeight="medium"> Dashboard</Text>
+                        <Link display="flex" alignItems="center">
+                            <Icon as={RiDashboardLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Dashboard</Text>
+                        </Link>
+                        <Link display="flex" alignItems="center">
+                            <Icon as={RiContactsLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Usuários</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+
+                <Box>
+                    <Text fontWeight="bold" color="gray.400" fontSize="small">LEADS</Text>
+                    <Stack spacing="4" mt="8" align="stretch">
+                        <Link display="flex" alignItems="center">
+                            <Icon as={RiInputMethodLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Formulários</Text>
+                        </Link>
+                        <Link display="flex" alignItems="center">
+                            <Icon as={RiGitMergeLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Clientes</Text>
                         </Link>
                     </Stack>
                 </Box>
